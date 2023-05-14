bd7cb170857ffcfe20a1153f35e0b2facab6f095
welquerjunio <welquerrodrigues.05@gmail.com>
2023-05-13 21:24:37 -0300

    ajustes gerais

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@ const localStorageKey = 'to-do-list-wj'
 
 //validation exist task
 function validateExistsNewTask(){
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
-    let inputValue = document.getElementById('input-new-task').value
-    let exists = values.find(x => x.name == inputValue)
+    const values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    const inputValue = document.getElementById('input-new-task').value
+    const exists = values.find(x => x.name == inputValue)
     return !exists ? false:true
 }
 //create new task
 function newTask(){
     let input = document.getElementById('input-new-task')
     input.style.border = ''
-
+    
     //validation
     if(!input.value){
         input.style.border = '1px solid red'
@@ -38,9 +38,9 @@ function showValues(){
     let list = document.getElementById('to-do-list')
     list.innerHTML = ''
     for(let i=0; i < values.length; i++){
-        list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onClick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
-      </svg></button></li>`
+        list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onClick='removeItem("${values[i]['name']}")'>
+        <img src="./img/remove-item.svg">
+        </button></li>`
     }
 
 }
